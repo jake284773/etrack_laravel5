d9cd19e98eadacd3104f32c2aa403b452d913293
Jake Moreman <mail@jakemoreman.co.uk>
2016-08-22 20:30:47 +0100

    Added bootforms for easy bootstrap forms.

--- config/app.php
+++ config/app.php
@@ -157,6 +157,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Laracasts\Flash\FlashServiceProvider::class,
+        AdamWathan\BootForms\BootFormsServiceProvider::class,
 
     ],
 
@@ -203,6 +204,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'BootForm' => AdamWathan\BootForms\Facades\BootForm::class,
 
     ],
 
