9156700084a6a84128f9dd7ac089da38a511c8f9
Jake Moreman <mail@jakemoreman.co.uk>
2016-08-22 19:15:23 +0100

    Sort subject sectors by number by default.

--- app/SubjectSector.php
+++ app/SubjectSector.php
@@ -2,9 +2,16 @@
 
 namespace App;
 
+use App\Scopes\NumberScope;
 use Illuminate\Database\Eloquent\Model;
 
 class SubjectSector extends Model
 {
-    //
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new NumberScope());
+    }
+
 }
