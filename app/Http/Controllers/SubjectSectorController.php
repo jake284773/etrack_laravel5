e554310a4c388e9c6a119344fb44d1984a3c4885
Jake Moreman <mail@jakemoreman.co.uk>
2016-08-23 21:03:53 +0100

    Added validation rules to subject sector controller

--- app/Http/Controllers/SubjectSectorController.php
+++ app/Http/Controllers/SubjectSectorController.php
@@ -9,6 +9,12 @@ use App\Http\Requests;
 
 class SubjectSectorController extends Controller
 {
+
+    protected $validationRules = [
+        'number' => 'required|unique:subject_sectors|integer',
+        'name'   => 'required',
+    ];
+
     /**
      * Display a listing of the resource.
      *
@@ -26,24 +32,30 @@ class SubjectSectorController extends Controller
      */
     public function create()
     {
-        //
+        return view('subject_sector.create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $validator = $this->validate($request, $this->validationRules);
+
+        if ($validator->fails()) {
+            return redirect('subject_sector.create')
+                ->withErrors($validator)
+                ->withInput();
+        }
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -54,7 +66,7 @@ class SubjectSectorController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -65,8 +77,8 @@ class SubjectSectorController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -77,7 +89,7 @@ class SubjectSectorController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
